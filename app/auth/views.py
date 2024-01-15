24fdac7319dc3d41ccefb4dc5afaf68bcfa5f0b0
Vachagan <vachagan.grigoryan@outlook.com>
2024-01-16 02:49:39 +0400

    Merge pull request #2
    * feat: implemented basic jwt auth and register

---
+++ app/auth/views.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.mixins import RetrieveModelMixin, CreateModelMixin
+
+from auth.serializers import UserSerializer, RegisterSerializer
+
+
+class UserViewSet(CreateModelMixin, RetrieveModelMixin, viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    serializer_register_class = RegisterSerializer
+
+    def get_serializer_class(self):
+        serializers = {
+            'retrieve': self.serializer_class,
+            'create': self.serializer_register_class,
+            'register': self.serializer_register_class,
+        }
+
+        return serializers.get(self.action, super().get_serializer_class())
+
+    @action(methods=["POST"], detail=False, url_path="register")
+    def register(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
